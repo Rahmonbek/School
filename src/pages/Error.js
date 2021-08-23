a53dd0b8c7f0ae949ac8bcedb0f38e2a361e9a98
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-08-23 17:35:04 +0500

    new error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,52 +1,111 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../css/Error.css'
-export default class componentName extends Component {
-  render() {
-    return (
-      <div className="errorT">
-          <a>
-  <header className="top-header">
-</header>
+import { Link } from "react-router-dom";
+import "../css/Error.css";
+import { data } from "./../server";
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 
+function SelectMap(props) {
+  // const [data, setData] = useState(Informations);
+  const [cities, setCities] = useState([]);
+  const [neighborhoods, setNeighborhood] = useState([]);
+  const [bool, setBool] = useState([true, false, false]);
 
-<div>
-  <div className="starsec"></div>
-  <div className="starthird"></div>
-  <div className="starfourth"></div>
-  <div className="starfifth"></div>
-</div>
+  const AllProvinces = [...new Set(data.map((item) => item.viloyat))];
 
+  useEffect(() => {
+    setCities([...new Set(data.map((item) => item.tuman))]);
+    setNeighborhood([...new Set(data.map((item) => item.nomi))]);
+  }, []);
 
+  const handleProvinceChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].viloyat === value) {
+        g.push(data[i]);
+      }
+    }
+    setCities([...new Set(g.map((item) => item.tuman))]);
+    setBool([true, true, false]);
+  };
+  const handleCityChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].tuman === value) {
+        g.push(data[i]);
+      }
+    }
+    setNeighborhood([...new Set(g.map((item) => item.nomi))]);
+    setBool([true, true, true]);
+  };
+  const handleNeighborhoodsChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].nomi === value) {
+        g.push(data[i]);
+      }
+    }
+  };
+  return (
+    <div className="map_select">
+      <div className="map_item">
+        {bool[0] ? (
+          <>
+            <h4>Viloyatlar</h4>
+            <ul className="school_ul1">
+              {AllProvinces.map((province) => (
+                <li key={province}>
+                  <Button
+                    type="primary"
+                    onClick={() => handleProvinceChange(province)}
+                  >
+                    {province}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          ""
+        )}
 
-<div className="lamp__wrap">
-  <div className="lamp">
-    <div className="cable"></div>
-    <div className="cover"></div>
-    <div className="in-cover">
-      <div className="bulb"></div>
-    </div>
-    <div className="light"></div>
-  </div>
-</div>
-
-<section className="error">
-  
-  <div className="error__content">
-    <div className="error__message message">
-      <h1 className="message__title">Sahifa topilmadi</h1>
-      <p className="message__text">Kechirasiz, siz izlagan sahifa bu erda topilmadi. Siz havola buzilgan bo'lishi mumkin yoki endi mavjud emas. Iltimos, qayta urinib ko'ring yoki biznikiga qarang.</p>
-    </div>
-    <div className="error__nav e-nav">
-      <a className="e-nav__link"><Link to="/uz">Bosh sahifaga qaytish</Link></a>
-    </div>
-  </div>
-  
-
-</section>
-
-  </a>
+        {bool[1] ? (
+          <>
+            <h4>Tumanalar</h4>
+            <ul className="school_ul2">
+              {cities.map((item, index) => (
+                <li key={index}>
+                  <Button type="primary" onClick={() => handleCityChange(item)}>
+                    {item}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          ""
+        )}
+        {bool[2] ? (
+          <>
+            <h4>Maktablar</h4>
+            <ul className="school_ul2">
+              {neighborhoods.map((item) => (
+                <li key={item}>
+                  <Button
+                    type="primary"
+                    onClick={() => handleNeighborhoodsChange(item)}
+                  >
+                    {item}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          ""
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default SelectMap;
