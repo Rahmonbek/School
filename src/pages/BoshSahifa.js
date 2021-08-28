1a7c23b2a6cbe34ebb11e51872ad8811389f1a6c
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-08-28 18:57:38 +0500

    t

--- src/pages/BoshSahifa.js
+++ src/pages/BoshSahifa.js
@@ -124,7 +124,7 @@ getSchool=()=>{
                     <Navbar collapseOnSelect expand="lg" >
                         <Container fluid style={{padding:'10px'}}>
                             <Navbar.Brand  style={{marginLeft:'150px'}}><p className='maktabLogo' style={{cursor:'pointer'}}>{this.state.school!==null?this.state.school.school_number+' - maktab':"Maktab raqami"}</p></Navbar.Brand>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{color: ' rgba(0, 0, 0, 0)',border:'none'}} ><i class="fa fa-bars" aria-hidden="true" style={{fontSize:'1.6rem',color:'white',position:'relative',top:'-10px'}}></i></Navbar.Toggle>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{marginLeft:'10px',color: ' rgba(0, 0, 0, 0)',border:'none'}} ><i class="fa fa-bars" aria-hidden="true" style={{fontSize:'1.6rem',color:'white',position:'relative',top:'-10px'}}></i></Navbar.Toggle>
                             <Navbar.Collapse id="responsive-navbar-nav">
                                 <Nav className={style.meauto}   >
                                     <NavLink style={{marginLeft:'20px', color:'white'}} to={`/${Global.user}`}><p className={style.navLink}>Bosh sahifa</p></NavLink>
