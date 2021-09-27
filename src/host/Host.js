329906ddae66f181515978f1603398bcd75bec30
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-09-27 17:02:29 +0500

    evwr

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://143.244.209.138:8000";
+export const url = "http://143.244.209.138";
 export const idMaktab = "3";
 export const user = "6";
 export const httpRequest = (config) => {
