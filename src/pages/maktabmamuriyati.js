a42ac18021cbe6d44f013e53c1166703aef8b905
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-01 00:34:25 +0500

    Update maktabmamuriyati.js

--- src/pages/maktabmamuriyati.js
+++ src/pages/maktabmamuriyati.js
@@ -56,14 +56,14 @@ getStaff=()=>{
           if(item1===5){
             orin2.push(item)
           }
-          if(item1===9){
+          if(item1===7){
             orin3.push(item)
           }
           if(item1===6){
             
             psixolog.push(item)
           }
-          if(item1===7){
+          if(item1===9){
             yetakchi.push(item)
           }
           if(item1===8){
